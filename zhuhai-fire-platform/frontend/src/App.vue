d85719afa3d97d5f4e9e14eeebd5f5adc04da8a4
fey-fang <fey-fang@users.noreply.github.com>
2026-04-15 09:12:27 +0000

    feat: 项目任务拆解与全流程开发
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- zhuhai-fire-platform/frontend/src/App.vue
+++ zhuhai-fire-platform/frontend/src/App.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <router-view />
 </template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html, body, #app {
+  width: 100%;
+  height: 100%;
+}
+</style>
